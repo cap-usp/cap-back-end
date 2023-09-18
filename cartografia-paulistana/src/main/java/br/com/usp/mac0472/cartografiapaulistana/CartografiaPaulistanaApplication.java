a4fb9ef6ef677fdb985d835ceeffd6bcf909c2ce
Gabriel Arrais <arrais.gbr@usp.br>
2023-09-18 16:44:19 -0300

    initial backend project

---
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/CartografiaPaulistanaApplication.java
@@ -0,0 +1,13 @@
+package br.com.usp.mac0472.cartografiapaulistana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartografiaPaulistanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartografiaPaulistanaApplication.class, args);
+	}
+
+}
---
+++ cartografia-paulistana/src/test/java/br/com/usp/mac0472/cartografiapaulistana/CartografiaPaulistanaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.usp.mac0472.cartografiapaulistana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartografiaPaulistanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

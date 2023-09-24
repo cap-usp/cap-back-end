80cd20f866ac2cebf6f26331e9bcf9c5b27ffe1f
Murilo André <40254653+MuriloAndre2000@users.noreply.github.com>
2023-09-24 18:10:22 -0300

    Code Improvments

--- front-end-vue/src/App.vue
+++ front-end-vue/src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>
-    <h3>Hello World
-    <br>
-    Oi Pessoal!
-    </h3>
+  <div id="app">
+    <h1>Latitude: {{ lat }}</h1>
+    <h1>Longitude: {{ lon }}</h1>
   </div>
-
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      lat: null,
+      lon: null
+    }
+  },
+  created() {
+    this.lat = this.$route.query.lat
+    this.lon = this.$route.query.lon
+  }
+}
+</script>
+
+<style>
+/* Add any CSS styling here */
+</style>

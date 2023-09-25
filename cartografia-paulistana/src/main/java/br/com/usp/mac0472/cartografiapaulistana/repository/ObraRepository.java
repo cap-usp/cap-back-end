6bdd07d683373d3986c33aa6cef692de216ebd0c
Vinicius Brisotti <vinicius.brisotti@hotmail.com>
2023-09-25 16:55:37 -0300

    Adding a missing notation

--- cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/repository/ObraRepository.java
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/repository/ObraRepository.java
@@ -2,6 +2,8 @@ package br.com.usp.mac0472.cartografiapaulistana.repository;
 
 import br.com.usp.mac0472.cartografiapaulistana.model.Obra;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ObraRepository extends JpaRepository<Obra, Integer> {
 }

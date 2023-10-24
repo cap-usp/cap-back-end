8f87cdd4ea06e9c2207b09f8ed3d3da440648cef
Gabriel Arrais <arrais.gbr@usp.br>
2023-10-24 19:58:52 -0300

    fix some imports

--- cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/controller/ConstrutoraController.java
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/controller/ConstrutoraController.java
@@ -1,59 +1,67 @@
 package br.com.usp.mac0472.cartografiapaulistana.controller;
 
-import br.com.usp.mac0472.cartografiapaulistana.model.Arquiteto;
-import br.com.usp.mac0472.cartografiapaulistana.model.Construtora;
-import br.com.usp.mac0472.cartografiapaulistana.service.ArquitetoService;
-import br.com.usp.mac0472.cartografiapaulistana.service.ConstrutoraService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import br.com.usp.mac0472.cartografiapaulistana.model.Construtora;
+import br.com.usp.mac0472.cartografiapaulistana.service.ConstrutoraService;
 
 @RestController
 @RequestMapping("/api/construtora")
 public class ConstrutoraController {
 
-    @Autowired
-    private ConstrutoraService service;
+	@Autowired
+	private ConstrutoraService service;
 
-    @GetMapping
-    public ResponseEntity<List<Construtora>> getConstrutoras() {
-        List<Construtora> construtoras = service.readConstrutoras();
-        return ResponseEntity.ok(construtoras);
-    }
-    @GetMapping("/{id}")
-    public ResponseEntity<Construtora> getConstrutora(@PathVariable Integer id) {
-        Construtora construtora = service.readConstrutora(id);
-        if (construtora != null) {
-            return ResponseEntity.ok(construtora);
-        }
-        return ResponseEntity.notFound().build();
-    }
+	@GetMapping
+	public ResponseEntity<List<Construtora>> getConstrutoras() {
+		List<Construtora> construtoras = service.readConstrutoras();
+		return ResponseEntity.ok(construtoras);
+	}
 
-    @PostMapping
-    public ResponseEntity<Construtora> createConstrutora(Construtora construtora) {
-        Construtora construtoraCreated = service.createConstrutora(construtora);
-        return ResponseEntity.status(HttpStatus.CREATED).body(construtoraCreated);
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<Construtora> getConstrutora(@PathVariable Integer id) {
+		Construtora construtora = service.readConstrutora(id);
+		if (construtora != null) {
+			return ResponseEntity.ok(construtora);
+		}
+		return ResponseEntity.notFound().build();
+	}
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Construtora> updateConstrutora(@PathVariable Integer id, @RequestBody Construtora construtora) {
-        Construtora updatedConstrutora = service.updateConstrutora(id, construtora);
-        if (updatedConstrutora != null) {
-            return ResponseEntity.ok(updatedConstrutora);
-        }
-        return ResponseEntity.notFound().build();
-    }
+	@PostMapping
+	public ResponseEntity<Construtora> createConstrutora(Construtora construtora) {
+		Construtora construtoraCreated = service.createConstrutora(construtora);
+		return ResponseEntity.status(HttpStatus.CREATED).body(construtoraCreated);
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteConstrutora(@PathVariable Integer id) {
-        boolean deleted = service.deleteConstrutora(id);
-        if (deleted) {
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
-    }
+	@PutMapping("/{id}")
+	public ResponseEntity<Construtora> updateConstrutora(@PathVariable Integer id,
+			@RequestBody Construtora construtora) {
+		Construtora updatedConstrutora = service.updateConstrutora(id, construtora);
+		if (updatedConstrutora != null) {
+			return ResponseEntity.ok(updatedConstrutora);
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteConstrutora(@PathVariable Integer id) {
+		boolean deleted = service.deleteConstrutora(id);
+		if (deleted) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
 
 }
--- cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/service/ConstrutoraService.java
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/service/ConstrutoraService.java
@@ -1,55 +1,54 @@
 package br.com.usp.mac0472.cartografiapaulistana.service;
 
-import br.com.usp.mac0472.cartografiapaulistana.model.Arquiteto;
-import br.com.usp.mac0472.cartografiapaulistana.model.Construtora;
-import br.com.usp.mac0472.cartografiapaulistana.repository.ArquitetoRepository;
-import br.com.usp.mac0472.cartografiapaulistana.repository.ConstrutoraRepository;
-import jakarta.transaction.Transactional;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import br.com.usp.mac0472.cartografiapaulistana.model.Construtora;
+import br.com.usp.mac0472.cartografiapaulistana.repository.ConstrutoraRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class ConstrutoraService {
 
-    @Autowired
-    private ConstrutoraRepository repository;
+	@Autowired
+	private ConstrutoraRepository repository;
 
-    public List<Construtora> readConstrutoras() {
-        return repository.findAll();
-    }
+	public List<Construtora> readConstrutoras() {
+		return repository.findAll();
+	}
 
-    public Construtora readConstrutora(Integer id) {
-        return repository.getReferenceById(id);
-    }
+	public Construtora readConstrutora(Integer id) {
+		return repository.getReferenceById(id);
+	}
 
-    @Transactional
-    public Construtora createConstrutora(Construtora construtora) {
-        return repository.save(construtora);
-    }
+	@Transactional
+	public Construtora createConstrutora(Construtora construtora) {
+		return repository.save(construtora);
+	}
 
-    @Transactional
-    public Construtora updateConstrutora(Integer id, Construtora updatedConstrutora) {
-        Construtora existingConstrutora = repository.getReferenceById(id);
+	@Transactional
+	public Construtora updateConstrutora(Integer id, Construtora updatedConstrutora) {
+		Construtora existingConstrutora = repository.getReferenceById(id);
 
-        if (existingConstrutora != null) {
-            existingConstrutora.update(updatedConstrutora);
-            return repository.save(existingConstrutora);
-        }
+		if (existingConstrutora != null) {
+			existingConstrutora.update(updatedConstrutora);
+			return repository.save(existingConstrutora);
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    @Transactional
-    public boolean deleteConstrutora(Integer id) {
-        Construtora construtora = repository.getReferenceById(id);
+	@Transactional
+	public boolean deleteConstrutora(Integer id) {
+		Construtora construtora = repository.getReferenceById(id);
 
-        if (construtora != null) {
-            repository.deleteById(id);
-            return true;
-        }
+		if (construtora != null) {
+			repository.deleteById(id);
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 }

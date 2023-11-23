9eafc21b6b8594fe1d349a6bc86defa03d213609
Gabriel Arrais <arrais.gbr@usp.br>
2023-11-23 10:44:14 -0300

    add referencia and endereco repositories

---
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package br.com.usp.mac0472.cartografiapaulistana.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.usp.mac0472.cartografiapaulistana.model.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}

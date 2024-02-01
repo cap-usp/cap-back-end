ac742e26d60510c3d5c5b3426dbe79fe6bd8494c
Gabriel Arrais <arrais.gbr@usp.br>
2024-01-31 23:17:51 -0300

    fix bugs modelmapper

--- cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/config/MapperConfig.java
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/config/MapperConfig.java
@@ -1,6 +1,8 @@
 package br.com.usp.mac0472.cartografiapaulistana.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +11,11 @@ public class MapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+		    .setFieldMatchingEnabled(true)
+		    .setFieldAccessLevel(AccessLevel.PRIVATE)
+		    .setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
 	}
 }

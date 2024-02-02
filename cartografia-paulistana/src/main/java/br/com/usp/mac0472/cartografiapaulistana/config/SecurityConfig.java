7576c5c1e4977d2d4e406ad9999db7bfbde1f44e
Gabriel Arrais <arrais.gbr@usp.br>
2024-02-02 02:06:26 -0300

    add cors configuration

--- cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/config/SecurityConfig.java
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,7 +28,7 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity.csrf(csrf -> csrf.disable())
+		return httpSecurity.csrf(csrf -> csrf.disable()).cors(Customizer.withDefaults())
 				.sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
 				.authorizeHttpRequests(authorize -> authorize
 						.requestMatchers("/error").permitAll()
--- cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/controller/AutenticacaoController.java
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/controller/AutenticacaoController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AutenticacaoController {
 
 	@Autowired

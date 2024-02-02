07fc681937ed6625e982d26ca71d93dc7d192e52
Gabriel Arrais <arrais.gbr@usp.br>
2024-02-02 03:33:13 -0300

    fix cors in controllers

--- cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/controller/ApiPublicaController.java
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/controller/ApiPublicaController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import br.com.usp.mac0472.cartografiapaulistana.service.ObraService;
 
 @RestController
 @RequestMapping("api/publica")
+@CrossOrigin(origins = "*")
 public class ApiPublicaController {
 	
 	@Autowired
--- cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/controller/ArquitetoController.java
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/controller/ArquitetoController.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/arquitetos")
+@CrossOrigin(origins = "*")
 public class ArquitetoController {
 
 	@Autowired

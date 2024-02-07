b68e18aaa8ae40586eda62eaf365643332f6df8d
Gabriel Arrais <arrais.gbr@usp.br>
2024-02-06 22:43:51 -0300

    delete unused dto

--- cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/controller/ApiPublicaController.java
+++ cartografia-paulistana/src/main/java/br/com/usp/mac0472/cartografiapaulistana/controller/ApiPublicaController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.usp.mac0472.cartografiapaulistana.dto.obra.ObraValidadaPageResponseDto;
+import br.com.usp.mac0472.cartografiapaulistana.dto.obra.ObraPageResponseDto;
 import br.com.usp.mac0472.cartografiapaulistana.model.Obra;
 import br.com.usp.mac0472.cartografiapaulistana.service.ObraService;
 
@@ -29,11 +29,11 @@ public class ApiPublicaController {
 	private ModelMapper mapper;
 
 	@GetMapping("/obras")
-	public ResponseEntity<Page<ObraValidadaPageResponseDto>> getValidadas(Pageable pageable) {
-		Page<Obra> obras = obraService.getValidadas(pageable);
-		List<ObraValidadaPageResponseDto> obrasDto = obras.stream()
-				.map(obra -> mapper.map(obra, ObraValidadaPageResponseDto.class)).toList();
-		Page<ObraValidadaPageResponseDto> response = PageableExecutionUtils.getPage(obrasDto, pageable,
+	public ResponseEntity<Page<ObraPageResponseDto>> getValidadas(Pageable pageable) {
+		Page<Obra> obras = obraService.readObras(pageable, true, true);
+		List<ObraPageResponseDto> obrasDto = obras.stream()
+				.map(obra -> mapper.map(obra, ObraPageResponseDto.class)).toList();
+		Page<ObraPageResponseDto> response = PageableExecutionUtils.getPage(obrasDto, pageable,
 				() -> obras.getTotalElements());
 		return ResponseEntity.ok(response);
 	}
